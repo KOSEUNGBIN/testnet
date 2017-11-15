06e143d4bca0f20115d549702872640629e3466b
Seung Bin . Ko <kosb15@naver.com>
2017-11-16 02:32:25 +0900

    Modify thread join

--- src/main/groovy/Testnet.groovy
+++ src/main/groovy/Testnet.groovy
@@ -3,22 +3,33 @@ import out.Outt
 
 class Testnet {
 
+    // Todo: gem 적용, Page 구성
     static void main(String[] args) {
 
         final def id = args[0]
         final def pw = args[1]
-
+        Thread t_admin, t_email, t_hip, t_inha, t_job, t_learn, t_minha, t_portal
         System.setProperty("webdriver.gecko.driver","/Users/koseungbin/Documents/chromedriver")
 
         try {
-            Thread.start(new admin().admin_start(id, pw)).join()
-            Thread.start(new email().email_start(id, pw)).join()
-            Thread.start(new hip().hip_start(id, pw)).join()
-            Thread.start(new inha().inha_start(id, pw)).join()
-            Thread.start(new job().job_start(id, pw)).join()
-            Thread.start(new learn().learn_start(id, pw)).join()
-            Thread.start(new minha().minha_start(id, pw)).join()
-            Thread.start(new portal().portal_start(id, pw)).join()
+            t_admin = Thread.start(new admin().admin_start(id, pw))
+            t_email = Thread.start(new email().email_start(id, pw))
+            t_hip = Thread.start(new hip().hip_start(id, pw))
+            t_inha = Thread.start(new inha().inha_start(id, pw))
+            t_job = Thread.start(new job().job_start(id, pw))
+            t_learn = Thread.start(new learn().learn_start(id, pw))
+            t_minha = Thread.start(new minha().minha_start(id, pw))
+            t_portal = Thread.start(new portal().portal_start(id, pw))
+
+            t_admin.join()
+            t_email.join()
+            t_hip.join()
+            t_inha.join()
+            t_job.join()
+            t_learn.join()
+            t_minha.join()
+            t_portal.join()
+
         }catch(Exception e) {
             Outt.out("TestNet Error!!")
         }
